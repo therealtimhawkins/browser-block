0629b302be30937dc7321cd227f29a8cb9249999
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-09-26 08:09:27 +0100

    project setup

--- src/network/Peer.vue
+++ src/network/Peer.vue
@@ -1,77 +1,107 @@
 <template>
   <div class="peer">
     <div>Hello Browser Coin</div>
-    <input v-model="incoming" />
-    <button @click="submit">Submit</button>
     <div>{{message}}</div>
-    <button class="handshake" @click="handshake">Join network...</button>
+    <button class="handshake" @click="requestHandshake">Join network...</button>
   </div>
 </template>
 
 <script>
 import Peer from "simple-peer";
 import * as axios from "axios";
+import * as uuid from "uuid/v1";
+import { find, get } from "lodash";
 
 export default {
   name: "Peer",
   data: function() {
     return {
-      outgoing: "",
-      incoming: "",
+      id: null,
       peer: null,
-      message: "",
-      polling: null
+      outgoing: null,
+      pollingHandshakes: null,
+      handshakeRequested: false,
+      incomingNode: [],
+      outgoingNodes: [],
+      message: ""
     };
   },
   created() {
-    this.peer = new Peer({
-      initiator: location.hash === "#1",
-      trickle: false
-    });
-
-    this.peer.on("signal", data => {
-      this.outgoing = JSON.stringify(data);
-    });
-
-    this.peer.on("connect", () => {
-      this.peer.send("Hi, welcome to browser coin.");
-    });
-
-    this.peer.on("data", data => {
-      this.message = data;
-    });
-
-    this.pollNodes();
+    this.id = uuid();
+    this.pollQueuedHandshakes();
+    this.initialPeer();
   },
   methods: {
-    submit: function() {
-      this.peer.signal(JSON.parse(this.incoming));
+    initialPeer: function() {
+      this.peer = new Peer({
+        initiator: location.hash === "#1",
+        trickle: false
+      });
+
+      this.peer.on("signal", data => {
+        this.outgoing = JSON.stringify(data);
+      });
+
+      this.peer.on("connect", () => {
+        this.peer.send("Hi, welcome to browser coin.");
+      });
+
+      this.peer.on("data", data => {
+        this.message = data;
+      });
     },
-    handshake: async function() {
+    handshakeResponse: async function(handshake) {
+      handshake.handshakeResponse = this.outgoing;
+      handshake.responseId = this.id;
+      console.log("handshake: ", handshake);
+      await axios.post("http://localhost:1992/handshake/response", handshake);
+    },
+    requestHandshake: async function() {
       await axios({
         method: "post",
         url: "http://localhost:1992/handshake",
         data: {
+          id: this.id,
           handshake: this.outgoing
         }
       });
+      this.handshakeRequested = true;
     },
-    pollNodes: function() {
-      this.polling = setInterval(async () => {
+    pollQueuedHandshakes: function() {
+      this.pollingHandshakes = setInterval(async () => {
         const response = await axios.get(
-          "http://localhost:1992/handshake/requested"
+          "http://localhost:1992/handshake/queued"
         );
 
-        response.data.forEach(handshake => {
-          if (handshake !== this.outgoing) {
-            console.log(handshake);
+        const allHandshakes = response.data;
+
+        if (this.handshakeRequested) {
+          const myHandshake = find(allHandshakes, ["requestId", this.id]);
+          console.log("myhandshake: ", myHandshake);
+        } else {
+          const handshake = allHandshakes.pop();
+          const requestId = get(handshake, "requestId");
+          if (requestId && requestId !== this.id) {
+            this.peer.signal(JSON.parse(handshake.handshake));
+            this.handshakeResponse(handshake);
           }
-        });
+        }
+
+        // response.data.forEach(response => {
+        //   if (response.handshake !== this.outgoing) {
+        //   } else {
+        //     console.log("handshake: ", response.handshake);
+        //     console.log("handshakeResponse: ", response.handshakeResponse);
+        //     if (response.handshakeResponse) {
+        //       this.handshakeResponse(response.handshakeResponse);
+        //     }
+        //   }
+        //
       }, 5000);
     }
   },
   beforeDestroy() {
-    clearInterval(this.polling);
+    clearInterval(this.pollingHandshakes);
   }
 };
 </script>
