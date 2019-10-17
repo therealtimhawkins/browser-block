64299e469904db46329685bb36a1abf52661d2e1
therealtimhawkins <timothyhaydenhawkins@gmail.com>
2019-10-17 18:47:18 +0100

    adds getPairedNodeIds function

--- src/services/network/index.js
+++ src/services/network/index.js
@@ -2,6 +2,7 @@ import Peer from 'peerjs'
 
 let peer
 const pairedNodes = []
+const pairedNodeIds = []
 
 export const initPeer = id => {
   peer = new Peer(id)
@@ -11,8 +12,13 @@ export const initPeer = id => {
 
 export const updatePairedNodes = node => {
   pairedNodes.push(node)
+  pairedNodeIds.push(node.id)
 }
 
 export const getPairedNodes = () => {
   return pairedNodes
+}
+
+export const getPairedNodeIds = () => {
+  return pairedNodeIds
 }
