6716eb68ebbb8b47a822eba4877872d495386bae
therealtimhawkins <timothyhaydenhawkins@gmail.com>
2019-10-17 18:45:07 +0100

    adds updatePairedNodes function

--- src/services/network/index.js
+++ src/services/network/index.js
@@ -1,9 +1,18 @@
-import Peer from "peerjs";
+import Peer from 'peerjs'
 
 let peer
+const pairedNodes = []
 
-export const initPeer = (id) => {
-  peer = new Peer(id);
+export const initPeer = id => {
+  peer = new Peer(id)
 
   return peer
+}
+
+export const updatePairedNodes = node => {
+  pairedNodes.push(node)
+}
+
+export const getPairedNodes = () => {
+  return pairedNodes
 }
